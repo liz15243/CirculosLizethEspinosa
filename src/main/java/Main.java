ff3a092174cec0f6993436291eb92af141de066f
liz15243 <lizethespi11@gmail.com>
2022-03-03 21:09:31 -0600

    Definiendo ya el Main con los metodos y atributos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+        Circulo objetocalculoUno = new Circulo();
+        objetocalculoUno.setRadio(4.5f);
+        objetocalculoUno.calcularArea();
+        objetocalculoUno.calcularCircunferencia();
+        objetocalculoUno.calcularDiametro();
+        objetocalculoUno.radio();
+
+
+    }
 
 }
