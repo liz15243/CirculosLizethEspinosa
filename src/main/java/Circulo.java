e2e37e93da4be8eb7b2fe0cc70a6d091772ed576
liz15243 <lizethespi11@gmail.com>
2022-03-03 20:15:54 -0600

    Creacion de la clase main y sus tres atributos

--- src/main/java/Circulo.java
+++ src/main/java/Circulo.java
@@ -1,2 +1,33 @@
 public class Circulo {
+    public double radio;
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+
+    public double calcularArea (){
+        double area;
+        area = (Math.pow(radio,2))*3.1416;
+
+        return area;
+    }
+    public double calcularDiametro (){
+        double diametro;
+        diametro = radio*2;
+
+        return diametro;
+    }
+public double calcularCircunferencia (){
+        double circunferencia;
+        circunferencia = calcularDiametro()*3.1416;
+
+        return circunferencia;
+}
+
+
+
 }
